5a62d3db6e914023fad63a18cd18eb88898431ac
skeiron <omar.work2@gmail.com>
2019-07-13 20:00:35 -0400

    made a json local storage

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,7 +1,20 @@
 var express = require('express');
-var router = express.Router();
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+// const { createReservation } = require('../firebase/firebaseUtilities');
+const date = require('date-and-time');
+const editJsonFile = require("edit-json-file");
+const Base64 = require('js-base64').Base64;
+
+let file = editJsonFile('./db/file.json');
+
+var router = express.Router();
 var dummyDB = {};
+
+// file related test START 
+
+// const now = new Date();
+// console.log(date.format(now, 'MM-DD-YY h-A'));
+// file related test END
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,13 +28,50 @@ router.get('/', function(req, res, next) {
 
 router.post('/sms', function(req, res, next) {
   const twiml = new MessagingResponse();
-  console.log({
-    message: req.body.Body,
-    from: req.body.From
-  })
+
+  // logic for updating the database from the request of the user 
+  // 1. make an object for the data and assign it to a var 
+  // 2. find the date as we'll need it
+  // 3. file.set(whatever, var) file.save() 
+  // format of message: 
+  // Reservation for NAME (first_last) at DATE TIME at RESTAURANT
+  let msg = req.body.Body.split(' ');
+  let name = `${msg[2]} ${msg[3]}`;
+  let reservation_date = msg[5];
+  let stringTime = msg[6];  
+  let ampm = stringTime.slice(stringTime.indexOf('m') - 1);
+  let time = parseInt(stringTime.slice(0, stringTime.indexOf('m') - 1));
+  let restaurant = msg[8];
+  let phoneNum = req.body.From;
+  let restaurant_id = Base64.encode(restaurant);
+  let now = new Date();
+  let formatNow = date.format(now, 'M-D-YY h-A');
+
+  
+
+  let request = {
+    [phoneNum]: {
+      [restaurant_id]: {
+        id: restaurant_id,
+        name,
+        reservation_date,
+        reservation_time: {
+          start: [time, ampm], end: [time + 1, ampm]
+        },
+        created_at: formatNow,
+        restaurant, 
+        phone_number: phoneNum
+      }
+    }
+  };
+  file.set("Persons", request);
+  file.save();
+
+
   twiml.message('Welcome to Reservation Bot, the Bot for Reservations!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  res.end(twiml.toString());  
+ 
 });
 
 router.get('/list', function(req, res, next) {
